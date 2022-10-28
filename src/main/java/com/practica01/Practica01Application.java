6a49e6d11cbcc6864a414d8605ff0d0a1bd4f436
nicomongeee <98415142+nicomongeee@users.noreply.github.com>
2022-10-27 22:07:50 -0600

    Arreglos

--- src/main/java/com/practica01/Practica01Application.java
+++ src/main/java/com/practica01/Practica01Application.java
@@ -1,13 +1,18 @@
 package com.practica01;
 
+import java.util.Calendar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Practica01Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Practica01Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Practica01Application.class, args);
+        Calendar c = Calendar.getInstance();
+        System.out.println("Date and Time :");
+        System.out.format("%tB %te, %tY%n", c, c, c);
+        System.out.format("%tl:%tM %tp%n", c, c, c);
+    }
 
 }
