dcf2137b99de41d3fe4e7abc0a24daf28e8fa8fe
nicomongeee <98415142+nicomongeee@users.noreply.github.com>
2022-10-27 22:53:49 -0600

    Arreglos

--- src/main/java/com/practica01/controller/IndexController.java
+++ src/main/java/com/practica01/controller/IndexController.java
@@ -4,6 +4,7 @@ import com.practica01.dao.EstadoDao;
 import com.practica01.domain.Estado;
 import com.practica01.services.EstadoService;
 import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
+@Slf4j
 public class IndexController {
     @Autowired
     
