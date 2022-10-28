0939d8497e4298bc04c341de99c9c07fb6f240f9
nicomongeee <98415142+nicomongeee@users.noreply.github.com>
2022-10-27 23:28:40 -0600

    Arreglos 2

--- src/main/java/com/practica01/services/EstadoServiceImpl.java
+++ src/main/java/com/practica01/services/EstadoServiceImpl.java
@@ -8,31 +8,35 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class EstadoServiceImpl implements EstadoService{
-    
-    
+public class EstadoServiceImpl implements EstadoService {
+
     @Autowired
     private EstadoDao estadoDao;
-    
+
+    //metodos para hacer un CRUD
+    //CREATE READ UPDATE DELETE
     @Override
     @Transactional(readOnly = true)
-    public List<Estado> getEstado(){
-        return (List<Estado>)estadoDao.findAll();
+    public List<Estado> getEstado() {
+        return (List<Estado>) estadoDao.findAll();
     }
+
     @Override
-    @Transactional(readOnly = true)
-    public Estado getEstado(Estado estado){
+    @Transactional(readOnly = true)//mientras tenga el readonly no modifica en la BD
+    public Estado getEstado(Estado estado) {
         return estadoDao.findById(estado.getIdEstado()).orElse(null);
-    }    
-    @Override
-    @Transactional
-    public void save(Estado estado){ 
-       estadoDao.save(estado);
     }
+
     @Override
     @Transactional
-    public void delete(Estado estado){
+    public void save(Estado estado) { //insertar o actualizar
+        estadoDao.save(estado);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Estado estado) {
         estadoDao.delete(estado);
-    }    
-   
+    }
+
 }
